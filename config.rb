e0e69ca8a9880d2d56ad3183d71e68713e060686
RYO NAKAE <ryo.n.1129@gmail.com>
2014-06-02 18:56:59 +0900

    Initial Release
    問題あったり使いにくかったら修正していく

---
+++ config.rb
@@ -0,0 +1,86 @@
+###
+# Compass
+###
+
+compass_config do |config|
+  config.output_style = :expanded
+  config.line_comments = false
+end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+###
+# Gem
+###
+# Slim Options
+require 'slim'
+set :slim, { :pretty => true, :sort_attrs => false, :format => :html5 }
+
+set :css_dir, 'css'
+set :js_dir, 'js'
+set :images_dir, 'img'
+set :fonts_dir, 'font'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+  set :relative_links, true
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+# Deploy
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+  deploy.branch = 'gh-pages'
+end
